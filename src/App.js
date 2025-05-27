e54455f4218f946aaea44802f812aeedaac26870
unknown <melkywafula29@gmail.com>
2025-05-27 08:53:15 +0300

    more updates

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import SearchComponent from './SearchComponent';
 function App() {
   return (
     <div className="App">
-      <h1>Search Engine App</h1>
       <SearchComponent />
     </div>
   );
